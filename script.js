973e0ca9b64fe3ec9260374582ec9e5ad07af6b2
Mehdi <mehdirefiyev177@gmail.com>
2023-07-02 18:17:09 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("change").addEventListener("click", change);
+function change(){
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://data.fixer.io/api/latest");
+    xhr.onload = function(){
+        if(this.status){
+            const response = JSON.parse(this.responseText);
+            const rate  = response.rates.TRY
+            const amount  = Number(document.getElementById("amount").value);
+            document.getElementById("tl").value =  amount * rate ;
+        }
+    }
+    xhr.send();
+}
